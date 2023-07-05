4f1a2ba4421fea66c7a2b5e8f48ffd507aaf9987
shreyans13 <shreyans1313@gmail.com>
2023-07-05 23:49:49 +0530

    init application

---
+++ src/App.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import Header from "./lib/components/Header.svelte";
+  // import svelteLogo from "./assets/svelte.svg";
+  // import Counter from "./lib/Counter.svelte";
+</script>
+
+<main>
+  <div class="app">
+    <!-- Header -->
+    <Header />
+    <p>Hello everyone</p>
+  </div>
+</main>
+
+<svelte:head>
+  <!-- Fonts -->
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+  />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
+  />
+
+  <!-- Material Typography -->
+  <link
+    rel="stylesheet"
+    href="https://unpkg.com/@material/typography@14.0.0/dist/mdc.typography.css"
+  />
+
+  <!-- SMUI -->
+  <link rel="stylesheet" href="https://unpkg.com/svelte-material-ui/bare.css" />
+</svelte:head>
+
+<style>
+  /* .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  } */
+</style>
---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,123 @@
+<script lang="ts" context="module">
+  import IconButton from "@smui/icon-button";
+  import Button, { Label, Icon } from "@smui/button";
+</script>
+
+<div class="header">
+  <img
+    class="header_logo"
+    src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+    alt="Amazon logo"
+    srcset=""
+  />
+  <div class="header_search">
+    <input type="text" class="header_searchInput" />
+    <!-- ICON BUTTON -->
+    <!-- <div style="display: flex; align-items: center;"> -->
+    <span class="header_searchIcon">
+      <Icon class="material-icons">search</Icon>
+    </span>
+    <!-- </div> -->
+    <!-- ICON BUTTON -->
+
+    <!-- LOGO -->
+  </div>
+  <div class="header_nav">
+    <!-- navigation 1  -->
+    <div class="header_option">
+      <span class="header_optionLineOne">Hello User</span>
+      <span class="header_optionLineTwo">Sign in </span>
+    </div>
+    <!-- navigation 2  -->
+    <div class="header_option">
+      <span class="header_optionLineOne">Return</span>
+      <span class="header_optionLineTwo">&Orders </span>
+    </div>
+    <!-- navigation 3  -->
+    <div class="header_option">
+      <span class="header_optionLineOne">your</span>
+      <span class="header_optionLineTwo">Prime</span>
+    </div>
+
+    <div class="header_optionBasket">
+      <Icon class="material-icons">add_shopping_cart</Icon>
+      <span class="header_optionLineTwo header_basketCount"> 0 </span>
+    </div>
+    <!-- header navigation -->
+  </div>
+</div>
+
+<style>
+  .header {
+    height: 60px;
+    display: flex;
+    /* will-change: filter; */
+    /* transition: filter 300ms; */
+    align-items: center;
+    background-color: #131921;
+    position: sticky;
+    top: 0;
+    z-index: 100;
+  }
+
+  .header_logo {
+    width: 100px;
+    object-fit: contain;
+    margin: 0 20px;
+    margin-top: 18px;
+  }
+
+  .header_search {
+    display: flex;
+    flex: 1;
+    align-items: center;
+    border-radius: 24px;
+  }
+
+  .header_searchInput {
+    height: 12px;
+    padding: 10px;
+    border: none;
+    width: 100%;
+  }
+  .header_searchIcon {
+    padding: 5px;
+    /* display: inline; */
+    background-color: #cd9042;
+    height: 22px;
+    width: 28px;
+  }
+
+  .header_nav {
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+  .header_option {
+    display: flex;
+    flex-direction: column;
+    color: white;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+
+  .header_optionLineOne {
+    font-size: 10px;
+  }
+
+  .header_optionLineTwo {
+    font-size: 13px;
+    font-weight: 800;
+  }
+
+  .header_optionBasket {
+    display: flex;
+    align-items: center;
+    color: white;
+  }
+
+  .header_basketCount {
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+</style>
